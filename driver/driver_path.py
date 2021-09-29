44fe8610e797b73614b5b75b819ad460598c88f3
dirtym0nk <dudechannelyt@gmail.com>
2021-09-29 15:35:44 +0300

    driver path

--- driver/driver_path.py
+++ driver/driver_path.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from dataclasses import dataclass
 
 
-driver = webdriver.Chrome('/home/alexey/Документы/pet_project/driver/chromedriver')
+# driver = webdriver.Chrome('/home/alexey/Документы/pet_project/driver/chromedriver')
+driver = webdriver.Chrome('../driver/chromedriver')
 # project_root = Path.cwd()
 
